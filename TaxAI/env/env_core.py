716212cc65bc8be17f19f9d9b6f6f716095976e5
Katsuki Oto <katsuki.oto@dena.com>
2024-04-09 03:32:40 +0900

    np.float -> float

--- TaxAI/env/env_core.py
+++ TaxAI/env/env_core.py
@@ -266,7 +266,7 @@ class economic_society:
     def gini_coef(self, wealths):
         cum_wealths = np.cumsum(sorted(np.append(wealths, 0)))
         sum_wealths = cum_wealths[-1]
-        xarray = np.array(range(0, len(cum_wealths))) / np.float(len(cum_wealths) - 1)
+        xarray = np.array(range(0, len(cum_wealths))) / float(len(cum_wealths) - 1)
         yarray = cum_wealths / sum_wealths
         B = np.trapz(yarray, x=xarray)
         A = 0.5 - B
